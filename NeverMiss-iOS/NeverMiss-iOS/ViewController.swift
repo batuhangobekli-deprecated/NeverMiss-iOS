d1a746d5e15b500ae5459dbe28117e1c8a87d7e7
batuhangobekli <btubatuhan@gmail.com>
2018-12-22 12:44:43 +0300

    first push

---
+++ NeverMiss-iOS/NeverMiss-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NeverMiss-iOS
+//
+//  Created by Batuhan Göbekli on 12/22/18.
+//  Copyright © 2018 Batuhan Göbekli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
