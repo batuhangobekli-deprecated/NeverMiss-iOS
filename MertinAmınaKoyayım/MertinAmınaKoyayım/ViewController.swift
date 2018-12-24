45aaa23ba6797ec45942480ddad37293d02d5156
batuhangobekli <btubatuhan@gmail.com>
2018-12-24 18:20:07 +0300

    Refactor

---
+++ MertinAmınaKoyayım/MertinAmınaKoyayım/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MertinAmınaKoyayım
+//
+//  Created by Batuhan Göbekli on 12/23/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
